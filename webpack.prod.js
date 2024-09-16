dd256c5f7f1ed03afe639742f762c561e0e2a969
Marchyus <marcius@marcius.lt>
2024-09-16 22:01:56 +0300

    config adjustment

---
+++ webpack.prod.js
@@ -0,0 +1,45 @@
+// path
+const path = require('path');
+
+// imports
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { watchFile } = require('fs');
+
+// config
+module.exports = {
+    mode: "production",
+    entry: "./src/static/js/main.js",
+
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/**/*"],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                type: "asset/resource",
+            },
+        ],
+    },
+}
